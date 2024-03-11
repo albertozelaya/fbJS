459423724af045f84e4c6a510f613c3d23894206
Alberto Zelaya <albertozelaya5@yahoo.com>
2024-03-10 22:41:07 -0600

    culminacion

--- index2.js
+++ index2.js
@@ -16,19 +16,39 @@ console.log(person.location?.city);
 //THEN ES TIPO CUADNO ESO FINALICE, ENTONCES(FUNCION HACER ALGO)
 //CADA BOJETO ES UNA PUBLCIACION
 //FETCH>THEN>RESPUESTA>DATA>POST>title o propiedad
-const ul = document.createElement("ul")
-fetch("https://jsonplaceholder.typicode.com/posts")
-  .then(function(respuesta){
-    return respuesta.json()
-  }).then((data)=>{
-    console.log(data);
-    data.forEach((post)=>{
-      const li = document.createElement("li")
-      li.innerText = post.title
-      ul.append(li)
-    })
-    document.body.append(ul)
+/* fetch("https://jsonplaceholder.typicode.com/posts")
+.then(function(respuesta){
+  return respuesta.json()
+}).then((data)=>{
+  console.log(data);
+  data.forEach((post)=>{
+    const li = document.createElement("li")
+    li.innerText = post.title
+    ul.append(li) //COMO QUE SE SUMA
   })
+  document.body.append(ul)
+}) */
 
 //PROMESA FETCH, CUANDO TERMINE RETORNAR ESA RESPUESTA EN JSON, Y CUANDO ESO TERMINE MOSTRAR LA DATA
-console.log('linea 2');
+
+const ol = document.createElement("ol")
+//console.log('linea 2');
+
+//ASYNC AWAIT AWAIT AL LADO DE FETCH ES PARA MARCAR LA ASINCRONIA
+const cargarData = async()=>{
+  const responde= await fetch("https://jsonplaceholder.typicode.com/posts") //se obtiene info
+  const data= await responde.json() //se convierte a json
+  
+  data.forEach((post)=>{ //por cada post, o sea cada array
+    const li = document.createElement("li") //se crea un elemento ol>li
+    li.innerText = post.title //dentro se pone el post>titolo --eso se extrae del post
+    ol.append(li) //COMO QUE SE SUMA 
+  })
+  
+  document.body.append(ol)
+}
+
+
+
+
+cargarData()
